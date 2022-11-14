d9ab59970315f4ff96cb9ff961d1ae9639f30fca
Nsilz44 <n_stbss@hotmail.com>
2022-11-14 13:52:15 +0000

    added the generation of intervals

--- instanceGenerator.py
+++ instanceGenerator.py
@@ -1,5 +1,18 @@
-import math
+import random
 
 # 
 def minimum_problem(num_of_intervals,salt_max,query_cost_uniformness,query_cost_max,probability_distribution):
-    
+    Li = []
+    current_point = 0
+    for i in range (0,num_of_intervals):
+        salt = random.uniform(0,salt_max)
+        current_point += salt
+        Li.append(current_point)
+    Ri =[]
+    for i in range (0,num_of_intervals):
+        salt = random.uniform(0,salt_max)
+        current_point += salt
+        Ri.append(current_point)
+    print(Li)
+    print(Ri)
+minimum_problem(10,10,0,0,0)
