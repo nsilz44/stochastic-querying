3243a87bbe49439118369f28faf7ebb5b3c2de88
Nsilz44 <n_stbss@hotmail.com>
2022-11-22 15:55:48 +0000

    finished generating hypergraph instance

--- instanceGenerator.py
+++ instanceGenerator.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 def minimumProblem(num_of_intervals,salt_max,query_cost_uniformness,query_cost_max,probability_distribution_list):
@@ -30,10 +31,18 @@ def hypergraphOrientationProblem(num_of_intervals,num_of_edges,max_vertices_per_
         salt = random.uniform(0,salt_max)
         current_point += salt
         Ri.append(current_point)
+    if query_cost_uniformness == 1:
+        Qi = [query_cost_max] * num_of_intervals
+    Ei = []
+    for i in range (0,num_of_edges):
+        verticesInEdge = random.randint(1,max_vertices_per_edge)
+        edge = list(np.random.permutation(np.arange(0,num_of_intervals))[:verticesInEdge])
+        Ei.append(edge)
     
     if query_cost_uniformness == 1:
         Qi = [query_cost_max] * num_of_intervals
-    return Li,Ri,Qi,probability_distribution_list
+    return Li,Ri,Ei,Qi,probability_distribution_list
 
-#p= 10 * [1]
+p= 10 * [1]
 #print(minimumProblem(10,10,1,1,p))
+#print(hypergraphOrientationProblem(10,8,3,10,1,1,p))
