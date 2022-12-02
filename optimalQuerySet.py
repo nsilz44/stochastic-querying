92505e6bfc636313af7ffbe8ff5512045fb064cd
Nsilz44 <n_stbss@hotmail.com>
2022-12-02 12:39:44 +0000

    refactored optimal query set

--- optimalQuerySet.py
+++ optimalQuerySet.py
@@ -1,25 +1,30 @@
 from probabilities import *
 from instanceGenerator import *
 from simulation import *
-def minimumProblemUniformOptimalQuerySet(Li,Ri,Vi):
+
+''' The algorithm to find optimal query set from section 3 Lemma 6
+Chaplick, S., Halldórsson, M. M., de Lima, M. S., & Tonoyan, T. (2021). 
+Query minimization under stochastic uncertainty. Theoretical Computer Science, 
+895, 75-95. https://doi.org/10.48550/arXiv.2010.03517 '''
+def minimumProblemOptimalQuerySet(Li,Ri,Vi,Qi):
     n = len(Li)
     min_value = min(Vi)
     i = 0
     query_set = []
+    cost = 0
     while Li[i] <= min_value:
         if Ri[i] >= min_value:
             query_set.append(i)
+            cost += Qi[i]
         i += 1
     remove_one = False
-    if len(query_set) == n:
-        remove_one = True
-        for v in Vi:
-            if v == Vi[0]:
-                continue
-            if v <= Ri[0]:
-                remove_one = False
-                break
-    if remove_one == True:
+    new_cost = 0
+    if min_value == Vi[0]:
+        for j in range(1,n):
+            new_cost += Qi[j]
+            if Vi[j] <= Ri[0]:
+                remove_one == True
+    if remove_one == True and new_cost < cost:
         del query_set[0]
     return query_set
 
@@ -29,6 +34,6 @@ def test():
     print('instance: ', m)
     s = minimumProblemSimulation(m[0],m[1],m[3])
     print('simulation: ', s)
-    q = minimumProblemUniformOptimalQuerySet(m[0],m[1],s)
+    q = minimumProblemOptimalQuerySet(m[0],m[1],s,p)
     print('query set: ', q)
 #test()    
