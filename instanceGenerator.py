c839a7bdc5f6f9ae60e6ad84e953b58890139987
Nsilz44 <n_stbss@hotmail.com>
2022-12-05 18:59:51 +0000

    changed the generator to make sure 0 isnt a salt so assumption holds

--- instanceGenerator.py
+++ instanceGenerator.py
@@ -1,32 +1,38 @@
 import random
 import numpy as np
 
+# function to exclude lower number from random.uniform
+def randomUniform(low,high):
+    i = 0
+    while i == 0:
+        i = random.uniform(low,high)
+    return i
 
-def minimumProblem(num_of_intervals,salt_max,Qi,probability_distribution_list):
+def minimumProblem(num_of_intervals,salt_max,Qi,Pi):
     Li = []
     current_point = 0
     for i in range (0,num_of_intervals):
-        salt = random.uniform(0,salt_max)
+        salt = randomUniform(0,salt_max)
         current_point += salt
         Li.append(current_point)
     Ri =[]
     for i in range (0,num_of_intervals):
-        salt = random.uniform(0,salt_max)
+        salt = randomUniform(0,salt_max)
         current_point += salt
         Ri.append(current_point)
     
-    return Li,Ri,Qi,probability_distribution_list
+    return Li,Ri,Qi,Pi
 
 def hypergraphOrientationProblem(num_of_intervals,num_of_edges,max_vertices_per_edge,salt_max,Qi,probability_distribution_list):
     Li = []
     current_point = 0
     for i in range (0,num_of_intervals):
-        salt = random.uniform(0,salt_max)
+        salt = randomUniform(0,salt_max)
         current_point += salt
         Li.append(current_point)
     Ri =[]
     for i in range (0,num_of_intervals):
-        salt = random.uniform(0,salt_max)
+        salt = randomUniform(0,salt_max)
         current_point += salt
         Ri.append(current_point)
     Ei = []
