db7ec7833b66b62731547ae9f8fc492d51821cab
Nsilz44 <n_stbss@hotmail.com>
2022-12-05 19:02:35 +0000

    fixed optimal query set to include the cases and stated assumption

--- optimalQuerySet.py
+++ optimalQuerySet.py
@@ -6,6 +6,9 @@ from simulation import *
 Chaplick, S., Halldórsson, M. M., de Lima, M. S., & Tonoyan, T. (2021). 
 Query minimization under stochastic uncertainty. Theoretical Computer Science, 
 895, 75-95. https://doi.org/10.48550/arXiv.2010.03517 '''
+
+
+# ASSUMPTION: Li[0] < Li[1] < ... < Li[n-1] < Ri[0] < Ri[1] < ... < Ri[n-1]
 def minimumProblemOptimalQuerySet(Li,Ri,Vi,Qi):
     # Does option a
     n = len(Li)
@@ -22,27 +25,30 @@ def minimumProblemOptimalQuerySet(Li,Ri,Vi,Qi):
             break
     # Checks whether option B is better
     remove_one = True
-    new_cost = 0
-    if min_value == Vi[0]:
-        for j in range(1,n):
-            new_cost += Qi[j]
-            # value which is a mandatory query
-            if Vi[j] <= Ri[0]:
-                remove_one == False
+    new_cost = sum(Qi) - Qi[0]
+    for j in range(1,n):
+        new_cost += Qi[j]
+        # value which is a mandatory query
+        if Vi[j] <= Ri[0]:
+            remove_one == False
+            break
     if remove_one == True and new_cost < cost:
-        del query_set[0]
+        query_set = list(range(1,(n)))
     return query_set
 
 def testOptionA():
     p= 10 * ['1']
-    m = minimumProblem(10,10,p,p)
+    l = 10 * [1]
+    m = minimumProblem(5,5,p,l)
     print('instance: ', m)
-    s = minimumProblemSimulation(m[0],m[1],m[3])
+    s = minimumProblemSimulation(m[0],m[1],m[2])
     print('simulation: ', s)
-    q = minimumProblemOptimalQuerySet(m[0],m[1],s,p)
+    q = minimumProblemOptimalQuerySet(m[0],m[1],s,l)
     print('query set: ', q)
 #testOptionA()    
 
 def testOptionB():
     print(minimumProblemOptimalQuerySet([10,11,12,13],[14,17,22,25],[13.5,15,20,22],[12,1,3,5]))
-testOptionB()
+    print(minimumProblemOptimalQuerySet([0,4,4.5,6],[10,20,21,22],[5,12,13,12],[5,1,1,1]))
+    print(minimumProblemOptimalQuerySet([0,2,6],[10,12,14],[5,4,12],[1,1,10]))
+#testOptionB()
