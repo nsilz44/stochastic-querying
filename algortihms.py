d2a55a14c87d2d32ae2f83c8c1833788869257b6
Nsilz44 <n_stbss@hotmail.com>
2023-01-05 20:20:19 +0000

    finished cascade

--- algortihms.py
+++ algortihms.py
@@ -18,7 +18,7 @@ def approximationAlgorithm(Li,Ri,Qi,Pi,Vi):
             tilt = True
             break
     if tilt == True:
-        if 
+        return tilt
     else:
         largest_Q = np.argmax(Qi)
         if wR/2 <= largest_Q and largest_Q <= 3/4 * wR:
@@ -35,7 +35,8 @@ def approximationAlgorithm(Li,Ri,Qi,Pi,Vi):
                         break
                 if workG != 0:
                     break
-        
+
+# cascade after querying I0 and sometimes previous ones      
 def cascade(query_set,Li,Ri,Qi,Pi,Vi):
     min_value = 1000000000000000000000000000000000000000000000000000
     for i in query_set:
@@ -46,10 +47,29 @@ def cascade(query_set,Li,Ri,Qi,Pi,Vi):
         if min_value > Vi[i]:
             min_value = Vi[i]
         del Vi[i]
-    while not True:
+    queries = 0
+    while len(Vi) != 0:
         # min value is smallest 
-        if len(Li) == 0:
-            break
         if min_value < Li[0]:
             break
+        if queries in query_set:
+            queries += 1
+            continue
+        else:
+            query_set.append(queries)
+            del Li[0]
+            del Ri[0]
+            del Qi[0]
+            del Pi[0]
+            if min_value > Vi[0]:
+                min_value = Vi[0]
+            del Vi[0]
+            queries += 1 
+    return query_set, min_value
+
+def testCascade():
+    finhl, lol = cascade([4,2],[1,2,2.5,3,4],[5,6,6.5,7,8],[1,1,1,1,1],[11,1,1,1,1],[3,5,2.6,6,7])
+    print(lol)
+    print(finhl)
         
+testCascade()
