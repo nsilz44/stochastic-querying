f1ef7247d3c0d758dca83ae7b11da51997b6a5b7
Nsilz44 <n_stbss@hotmail.com>
2023-01-08 13:11:37 +0000

    added calculation of uniform prob

--- probabilities.py
+++ probabilities.py
@@ -3,10 +3,19 @@ import random
 def uniformProb(l_endpoint,r_endpoint):
     return random.uniform(l_endpoint,r_endpoint)
 
+def calcUniformProb(L_endpoint,r_endpoint,range):
+    return (r_endpoint - L_endpoint)/range 
+
 def prob(probability_type,L_endpoint,r_endpoint):
     switch={
     '1': uniformProb(L_endpoint,r_endpoint),
        }
     return switch.get(probability_type,'not available')
 
+def calcProb(probability_type,L_endpoint,r_endpoint,range):
+    switch={
+    '1': calcUniformProb(L_endpoint,r_endpoint,range),
+       }
+    return switch.get(probability_type,'not available')
 #print(prob('1',10,20))
+#print(calcProb('1',10,20,15))
